954b231c7aaf228afe6ee664450875ea21ab8d85
Murat Kozan <muratkozan350@gmail.com>
2019-03-27 20:17:17 +0200

    kuntao: Build lineage trust hal

--- hidl.mk
+++ hidl.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
